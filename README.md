227d526ce7e3ca431d824d6ed83bf19e6b0018e3
kuuube <hexagonisalie@gmail.com>
2022-04-10 10:33:00 -0400

    Feat: Add Disable Timer Repeating

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Prevents unintended repitition of pen tip inputs.
 
 **Debounce Timer:** The time in ms after input pressure goes below the **Pressure Threshold** and subsequent inputs are extended, combined, or dropped.
 
-**Pressure Threshold:** The raw pen pressure value at which inputs are handled. 
+**Pressure Threshold:** The raw pen pressure value at which debounce is applied.
 
 - **Default Action:** Output pressure will be held at or above the set **Pressure Threshold** after an input goes below the **Pressure Threshold** until the time set in the **Debounce Timer** has passed. 
 
@@ -17,6 +17,8 @@ Prevents unintended repitition of pen tip inputs.
     Subsequent inputs above the **Pressure Threshold** within the time set in the **Debounce Timer** will be dropped and will reset the **Debounce Timer** until the pressure is below the **Pressure Threshold**.
 
 **Drop Excess Inputs:** Instead of extending or combining subsequent inputs, all subsequent inputs above the **Pressure Threshold** and within the time set in the **Debounce Timer** are dropped.
+
+**Disable Timer Repeating:** The timer is not reset for each subsequent press within the time set in the Debounce Timer.
 
 <br>
 
--- Tablet_Debounce/Tablet_Debounce.cs
+++ Tablet_Debounce/Tablet_Debounce.cs
@@ -18,13 +18,16 @@ namespace Pressure_Debounce
                 if (!Drop_excess)
                 {
                     if (tabletReport.Pressure < Pressure_threshold)
+                    //pressure less than threshold
                     {
                         if (debounceStopwatch.Elapsed.TotalMilliseconds <= Debounce_timer)
+                        //pressure less than threshold and elapsed time less than or equal to debounce timer
                         {
                             tabletReport.Pressure = last_pressure;
                             return input;
                         }
                         else
+                        //pressure less than threshold and elapsed time greater than debounce timer
                         {
                             debounceStopwatch.Restart();
                             last_pressure = tabletReport.Pressure;
@@ -32,8 +35,12 @@ namespace Pressure_Debounce
                         }
                     }
                     else
+                    //pressure greater than threshold
                     {
-                        debounceStopwatch.Restart();
+                        if (!Disable_timer)
+                        {
+                            debounceStopwatch.Restart();
+                        }
                         last_pressure = tabletReport.Pressure;
                         return input;
                     }
@@ -41,21 +48,29 @@ namespace Pressure_Debounce
                 else
                 {
                     if (tabletReport.Pressure > Pressure_threshold)
+                    //pressure greater than thereshold
                     {
                         if (debounceStopwatch.Elapsed.TotalMilliseconds <= Debounce_timer)
+                        //pressure greater than threshold and elapsed time less than or equal to debounce timer
                         {
                             if (last_pressure < Pressure_threshold)
+                            //pressure greater than threshold, elapsed time less than or equal to debounce timer, and last pressure less than threshold
                             {
-                                debounceStopwatch.Restart();
+                                if (!Disable_timer)
+                                {
+                                    debounceStopwatch.Restart();
+                                }
                                 tabletReport.Pressure = 0;
                                 return input;
                             }
                             else
+                            //pressure greater than threshold, elapsed time less than or equal to debounce timer, and last pressure greater or equal to threshold
                             {
                                 return input;
                             }
                         }
                         else
+                        //pressure greater than threshold and elapsed time greater than debounce timer
                         {
                             last_pressure = tabletReport.Pressure;
                             debounceStopwatch.Restart();
@@ -63,6 +78,7 @@ namespace Pressure_Debounce
                         }
                     }
                     else
+                    //pressure less than threshold
                     {
                         last_pressure = tabletReport.Pressure;
                         return input;
@@ -94,17 +110,22 @@ namespace Pressure_Debounce
 
         [Property("Debounce Timer"), Unit("ms"), DefaultPropertyValue(30f), ToolTip
             ("Tablet Debounce:\n\n" +
-            "Debounce Timer: The time after an input when subsequent inputs are handled.")]
+            "Debounce Timer: The time in ms after input pressure goes below the Pressure Threshold and subsequent inputs are extended, combined, or dropped.")]
         public float Debounce_timer { set; get; }
 
         [Property("Pressure Threshold"), DefaultPropertyValue(1f), ToolTip
             ("Tablet Debounce:\n\n" +
-            "Pressure Threshold: The raw pen pressure value at which inputs are handled.")]
+            "Pressure Threshold: The raw pen pressure value at which debounce is applied.")]
         public float Pressure_threshold { set; get; }
 
         [BooleanProperty("Drop Excess Inputs", ""), ToolTip
             ("Tablet Debounce:\n\n" +
-            "Drop Excess Inputs: Instead of combining subsequent inputs within the debounce ranges, the inputs are dropped.")]
+            "Drop Excess Inputs: Instead of extending or combining subsequent inputs, all subsequent inputs above the Pressure Threshold and within the time set in the Debounce Timer are dropped.")]
         public bool Drop_excess { set; get; }
+
+        [BooleanProperty("Disable Timer Repeating", ""), ToolTip
+        ("Tablet Debounce:\n\n" +
+        "Disable Timer Repeating: The timer is not reset for each subsequent press within the time set in the Debounce Timer.")]
+        public bool Disable_timer { set; get; }
     }
 }
